1e028486235d2217bd311584759a82949970b359
Minoru <miyarappo@gmail.com>
2018-01-05 18:05:39 +0900

    todo機能を実装

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
+import './css/App.css'
+
 
 class App extends Component {
+
+  constructor() {
+    super()
+    const todos = [
+      {
+        id: 1,
+        title: "Hello, React!",
+        desc: "React始めました",
+        done: false
+      },
+      {
+        id: 2,
+        title: "Hello, Redux!",
+        desc: "Reduxも始めました",
+        done: false
+      },
+    ]
+    this.state = {
+      todos: todos,
+      countTodo: todos.length + 1,
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const desc = e.target.desc.value;
+    const todos = this.state.todos.slice()
+    const countTodo = this.state.countTodo
+
+    todos.push({
+      id: countTodo,
+      title: title,
+      desc: desc,
+      done: false,
+    });
+
+    this.setState({ todos })
+    this.setState({ countTodo: countTodo + 1 })
+
+
+    e.target.title.value = '';
+    e.target.desc.value = '';
+  }
+
+  setTodoStatus(clickTodo) {
+    const todos = this.state.todos.slice();
+    const todo = todos[clickTodo.id -1];
+    todo.done = !todo.done;
+    todos[clickTodo.id - 1] = todo;
+
+    this.setState({ todos })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <h1>todoアプリを作ってみた</h1>
+        <Form handleSubmit={this.handleSubmit.bind(this)} />
+        <TodoList
+          todos={this.state.todos}
+          setTodoStatus={this.setTodoStatus.bind(this)}
+          />
       </div>
     );
   }
 }
 
-export default App;
+export default App
